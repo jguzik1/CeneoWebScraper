819b9ee46d00f09183c58a138c13786aa1f3420a
jguzik1 <227616@student.uek.krakow.pl>
2023-05-31 19:57:05 +0200

    templates Jakub Guzik

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from app import app
+from flask import Flask, render_template
 
 @app.route("/")
+def home():
+    return render_template('main.html')
 @app.route("/index")
 def index():
     return "Hello World"
