0bb710c14fd20aa22e775ff5b985468813e9426c
jguzik1 <227616@student.uek.krakow.pl>
2023-06-07 14:29:30 +0200

    JG 7.06.23

--- app/routes.py
+++ app/routes.py
@@ -2,13 +2,18 @@ from app import app
 from flask import Flask, render_template
 
 @app.route("/")
-def home():
-    return render_template('main.html')
-@app.route("/index")
-def index():
-    return "Hello World"
+@app.route("/main")
+def main():
+    return render_template("main.html")
 
-@app.route("/name", defaults={'name': "Anonim"})
-@app.route("/name/<name>")
-def name(name):
-    return f"Hello {name}"
+@app.route("/extraction")
+def extraction():
+    return render_template("extraction.html")
+
+@app.route("/product_list")
+def product_list():
+    return render_template("p_list.html")
+
+@app.route("/product")
+def product():
+    return render_template("product.html")
